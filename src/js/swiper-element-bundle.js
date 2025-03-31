67faedc7b99b16c2aa03944d026f597427f1d0b2
SStakhovski <sstahovsky@insoftgroup.com>
2025-03-31 15:47:14 +0200

    check for iframe

--- src/js/swiper-element-bundle.js
+++ src/js/swiper-element-bundle.js
@@ -9998,8 +9998,11 @@
   const ClassToExtend = typeof window === 'undefined' || typeof HTMLElement === 'undefined' ? DummyHTMLElement : HTMLElement;
   const arrowSvg = `<svg width="11" height="20" viewBox="0 0 11 20" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M0.38296 20.0762C0.111788 19.805 0.111788 19.3654 0.38296 19.0942L9.19758 10.2796L0.38296 1.46497C0.111788 1.19379 0.111788 0.754138 0.38296 0.482966C0.654131 0.211794 1.09379 0.211794 1.36496 0.482966L10.4341 9.55214C10.8359 9.9539 10.8359 10.6053 10.4341 11.007L1.36496 20.0762C1.09379 20.3474 0.654131 20.3474 0.38296 20.0762Z" fill="currentColor"/></svg>
     `;
+  const renderedInIFrame = () => {
+    return window && window.parent && window.location !== window.parent.location;
+  }
   const addStyle = (shadowRoot, styles) => {
-    if (typeof CSSStyleSheet !== 'undefined' && shadowRoot.adoptedStyleSheets) {
+    if (typeof CSSStyleSheet !== 'undefined' && shadowRoot.adoptedStyleSheets && !renderedInIFrame()) {
       const styleSheet = new CSSStyleSheet();
       styleSheet.replaceSync(styles);
       shadowRoot.adoptedStyleSheets = [styleSheet];
