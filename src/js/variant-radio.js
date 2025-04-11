b46e77e41e6e468453a0b825726654df2d066408
SStakhovski <sstahovsky@insoftgroup.com>
2025-04-11 11:21:08 +0200

    clicks

--- src/js/variant-radio.js
+++ src/js/variant-radio.js
@@ -38,46 +38,14 @@ export class VariantRadio extends HTMLElement {
           const productSlug = trigger.getAttribute("data-product-slug");
           const variantId = trigger.getAttribute("data-variant-id");
           const sectionId = trigger.getAttribute("data-section-id");
-          await this.updateProductSelectedVariant(productSlug, variantId, sectionId);
-          this.updateSelectedVariant(input);
-        }
-
-        if (this.cartDrawerButton) {
-          const variantId = trigger.getAttribute("data-variant-id");
-          this.cartDrawerButton.setAttribute("data-variant-id", variantId);
+          await this.selectVariant(productSlug, variantId, sectionId);
         }
       });
     });
   }
 
-  updateSelectedVariant(selectedInput) {
-    // Update the state of the radio inputs and triggers
-    this.triggers.forEach((trigger) => {
-      const inputId = trigger.getAttribute("aria-labelledby");
-      // const input = this.querySelector(`#${inputId}`);
-      const input = document.getElementById(inputId);
 
-      if (input && !input.disabled) {
-        if (input === selectedInput && input.checked) {
-          trigger.classList.add("bg-[#3B0B93]", "text-white");
-          trigger.classList.remove(
-            "border-black",
-            "hover:bg-black",
-            "hover:text-white"
-          );
-        } else {
-          trigger.classList.remove("bg-[#3B0B93]", "text-white");
-          trigger.classList.add(
-            "border-black",
-            "hover:bg-black",
-            "hover:text-white"
-          );
-        }
-      }
-    });
-  }
-
-  updateProductItem(html, selector) {
+  updateProductElement(html, selector) {
     const currentItem = document.querySelector(selector);
     const newItem = html.querySelector(selector);
     if (currentItem && newItem) {
@@ -85,32 +53,31 @@ export class VariantRadio extends HTMLElement {
     }
   }
 
-  async updateProductSelectedVariant(productSlug, variantId, sectionId) {
+  async selectVariant(productSlug, variantId, sectionId) {
     const requestUrl = `/products/${productSlug}?section=${"page__products_product__product"}&variant=${variantId}`;
+    this.abortController?.abort();
     this.abortController = new AbortController();
 
-    await fetch(requestUrl, { signal: this.abortController.signal })
-      .then((response) => response.text())
-      .then((responseText) => {
-        const html = new DOMParser().parseFromString(responseText, 'text/html');
-        return html;
-      })
-      .then((html) => {
-        // update items individually to keep scroll position and opened accordion items
-        this.updateProductItem(html, "product-section #product-price");
-        this.updateProductItem(html, "product-section quantity-selector-root");
-        this.updateProductItem(html, "product-section product-variant-options");
-      })
-      .then(() => {
-        // set focus to last clicked option value
-       //  document.querySelector(`#${targetId}`)?.focus();
-      })
-      .catch((error) => {
-        if (error.name === 'AbortError') {
-          console.log('Fetch aborted by user');
-        } else {
-          console.error(error);
-        }
-      });
+    try {
+      const response = await fetch(requestUrl, { signal: this.abortController.signal });
+      const responseText = await response.text();
+      const html = new DOMParser().parseFromString(responseText, 'text/html');
+
+      // update elements individually to keep scroll position, selected image and opened accordion items
+      this.updateProductElement(html, "product-section #product-price");
+      this.updateProductElement(html, "product-section quantity-selector-root > div > input");
+      this.updateProductElement(html, "product-section product-variant-options");
+
+      if (this.cartDrawerButton) {
+        const variantId = trigger.getAttribute("data-variant-id");
+        this.cartDrawerButton.setAttribute("data-variant-id", variantId);
+      }
+    } catch(error) {
+      if (error.name === 'AbortError') {
+        console.log('Fetch aborted by user');
+      } else {
+        console.error(error);
+      }
+    }
   }
 }
