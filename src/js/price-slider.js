c245fb31d10e7c36095f108f5a4a676ae0e6eb99
daniel.wiley <daniel.wiley@swell.is>
2025-07-10 13:07:29 -0600

    nouislider.js file added

--- src/js/price-slider.js
+++ src/js/price-slider.js
@@ -1,4 +1,4 @@
-import noUiSlider from "nouislider";
+import noUiSlider from "./nouislider.js";
 
 export function initPriceSliders() {
   document.querySelectorAll(".price-slider").forEach(function (container) {
