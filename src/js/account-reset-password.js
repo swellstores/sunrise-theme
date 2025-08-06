5e060fda6a09ff04e8812d3ae8e5066cc4163749
SergeySwell <sergey@swell.is>
2025-08-06 16:17:11 +0200

    update event

--- src/js/account-reset-password.js
+++ src/js/account-reset-password.js
@@ -6,11 +6,11 @@ export class AccountResetPassword extends HTMLElement {
   }
 
   connectedCallback() {
-    this.addEventListener("keyup", this.onInputChangedBound);
+    this.addEventListener("input", this.onInputChangedBound);
   }
 
   disconnectedCallback() {
-    this.removeEventListener("keyup", this.onInputChangedBound);
+    this.removeEventListener("input", this.onInputChangedBound);
   }
 
   onInputChanged() {
