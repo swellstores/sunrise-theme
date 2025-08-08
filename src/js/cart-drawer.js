19db6c417c2fe42a838d182219f59eb05221a237
Vlad Studenichnik <vstudenichnik@insoftgroup.com>
2025-08-08 18:45:58 +0200

    Account nav and details

--- src/js/cart-drawer.js
+++ src/js/cart-drawer.js
@@ -1,3 +1,4 @@
+import { DrawerPanel } from "./drawer-panel";
 import {
   CartItemRemoveEvent,
   CartItemQuantityChangeEvent,
@@ -10,49 +11,34 @@ import eventBus from "./utils/event-bus";
  *
  * @export
  * @class CartDrawer
- * @extends {HTMLElement}
+ * @extends {DrawerPanel}
  */
-export class CartDrawer extends HTMLElement {
+export class CartDrawer extends DrawerPanel {
   constructor() {
     super();
 
     this.trigger = null;
     this.drawerTriggers = [];
-    this.backdropOverlay = null;
-
-    this.touchStartX = 0;
-    this.touchEndX = 0;
-    this.swipeThreshold = 50; // Minimum distance for a swipe
 
     this.subscriptions = [];
 
     this.openBound = this.open.bind(this);
     this.closeBound = this.close.bind(this);
-    this.onDocumentKeyDownBound = this.onDocumentKeyDown.bind(this);
 
     this.onAddItemBound = this.onAddItem.bind(this);
     this.onChangeItemQuantityBound = this.onChangeItemQuantity.bind(this);
     this.onRemoveItemBound = this.onRemoveItem.bind(this);
     this.onBeforeUpdateCartBound = this.onBeforeUpdateCart.bind(this);
     this.onAfterUpdateCartBound = this.onAfterUpdateCart.bind(this);
-
-    this.handleTouchStartBound = this.handleTouchStart.bind(this);
-    this.handleTouchMoveBound = this.handleTouchMove.bind(this);
-    this.handleTouchEndBound = this.handleTouchEnd.bind(this);
   }
 
   connectedCallback() {
-    this.setAttribute("role", "dialog");
+    super.connectedCallback();
 
     this.trigger = document.querySelector("cart-trigger");
-    this.backdropOverlay = document.querySelector("backdrop-root");
 
     if (this.trigger) {
       this.trigger.addEventListener("click", this.openBound);
-    }
-
-    if (this.backdropOverlay) {
-      this.backdropOverlay.addEventListener("click", this.closeBound);
     }
 
     this.connectDrawerTriggers();
@@ -63,14 +49,6 @@ export class CartDrawer extends HTMLElement {
     );
     this.addEventListener(CartItemRemoveEvent.type, this.onRemoveItemBound);
 
-    // Close cart drawer on escape key press
-    document.addEventListener("keydown", this.onDocumentKeyDownBound);
-
-    // Add touch event listeners
-    this.addEventListener("touchstart", this.handleTouchStartBound);
-    this.addEventListener("touchmove", this.handleTouchMoveBound);
-    this.addEventListener("touchend", this.handleTouchEndBound);
-
     this.subscriptions.push(
       eventBus.on("cart-item-add", this.onAddItemBound),
       eventBus.on("cart-update-before", this.onBeforeUpdateCartBound),
@@ -79,12 +57,10 @@ export class CartDrawer extends HTMLElement {
   }
 
   disconnectedCallback() {
+    super.disconnectedCallback();
+
     if (this.trigger) {
       this.trigger.removeEventListener("click", this.openBound);
-    }
-
-    if (this.backdropOverlay) {
-      this.backdropOverlay.removeEventListener("click", this.closeBound);
     }
 
     this.disconnectDrawerTriggers();
@@ -94,13 +70,6 @@ export class CartDrawer extends HTMLElement {
       this.onChangeItemQuantityBound
     );
     this.removeEventListener(CartItemRemoveEvent.type, this.onRemoveItemBound);
-
-    document.removeEventListener("keydown", this.onDocumentKeyDownBound);
-
-    // Remove touch event listeners
-    this.removeEventListener("touchstart", this.handleTouchStartBound);
-    this.removeEventListener("touchmove", this.handleTouchMoveBound);
-    this.removeEventListener("touchend", this.handleTouchEndBound);
 
     this.subscriptions.forEach((unsubscribe) => unsubscribe());
     this.subscriptions = [];
@@ -121,33 +90,11 @@ export class CartDrawer extends HTMLElement {
   }
 
   open() {
-    if (!this.backdropOverlay) {
-      return;
-    }
-
-    this.backdropOverlay.classList.remove("translate-x-full");
-    this.classList.remove("translate-x-full");
-    this.setAttribute("aria-hidden", "false");
+    super.open();
   }
 
   close() {
-    if (!this.backdropOverlay) {
-      return;
-    }
-
-    this.backdropOverlay.classList.add("translate-x-full");
-    this.classList.add("translate-x-full");
-    this.setAttribute("aria-hidden", "true");
-  }
-
-  /** @param {KeyboardEvent} event */
-  onDocumentKeyDown(event) {
-    if (
-      event.key === "Escape" &&
-      this.getAttribute("aria-hidden") === "false"
-    ) {
-      this.closeBound();
-    }
+    super.close();
   }
 
   disableItem(line) {
@@ -217,46 +164,5 @@ export class CartDrawer extends HTMLElement {
     // Reset drawer trigger events
     this.disconnectDrawerTriggers();
     this.connectDrawerTriggers();
-  }
-
-  /**
-   * Handle touch start, move, and end events
-   */
-  handleTouchStart(event) {
-    this.touchStartX = event.touches[0].clientX;
-  }
-
-  handleTouchMove(event) {
-    if (this.getAttribute("aria-hidden") === "true") {
-      return;
-    }
-
-    const touchX = event.touches[0].clientX;
-    const diff = this.touchStartX - touchX;
-
-    // Only allow right swipes
-    if (diff < 0) {
-      event.preventDefault();
-      this.style.transform = `translateX(${Math.abs(diff)}px)`;
-    }
-  }
-
-  handleTouchEnd(event) {
-    if (this.getAttribute("aria-hidden") === "true") {
-      return;
-    }
-
-    this.touchEndX = event.changedTouches[0].clientX;
-    const diff = this.touchStartX - this.touchEndX;
-    const absDiff = Math.abs(diff);
-
-    this.style.transform = "";
-
-    if (absDiff > this.swipeThreshold) {
-      if (diff < 0) {
-        // Swipe right - close menu
-        this.closeBound();
-      }
-    }
   }
 }
