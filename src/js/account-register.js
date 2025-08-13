0d40ebcacaeb85449537c178f0e86495d0a2a3d1
SergeySwell <sergey@swell.is>
2025-08-13 14:59:13 +0200

    register form

--- src/js/account-register.js
+++ src/js/account-register.js
@@ -1,37 +1,85 @@
+import { getFormData, isFormChanged, isFormValid } from "./utils/form";
+
+const FIELDS = Object.freeze([
+  "customer[first_name]",
+  "customer[last_name]",
+  "customer[email]",
+  "customer[password]",
+]);
+
 export class AccountRegister extends HTMLElement {
   constructor() {
     super();
 
-    this.onInputChangedBound = this.onInputChanged.bind(this);
+    this.form = null;
+    this.submitButton = null;
+    this.initialFormData = null;
+    this.fields = {};
+
+    this.onInputChangeBound = this.onInputChange.bind(this);
+    this.onBeforeSwapBound = this.onBeforeSwap.bind(this);
+    this.onAfterSwapBound = this.onAfterSwap.bind(this);
   }
 
   connectedCallback() {
-    this.addEventListener("input", this.onInputChangedBound);
+    this.form = this.querySelector("form");
+    this.submitButton = this.querySelector('button[type="submit"]');
+    this.initialFormData = getFormData(this.form, FIELDS);
+
+    this.addEventListener("input", this.onInputChangeBound);
+    this.addEventListener("htmx:beforeSwap", this.onBeforeSwapBound);
+    this.addEventListener("htmx:afterSwap", this.onAfterSwapBound);
   }
 
   disconnectedCallback() {
-    this.removeEventListener("input", this.onInputChangedBound);
+    this.removeEventListener("input", this.onInputChangeBound);
+    this.removeEventListener("htmx:beforeSwap", this.onBeforeSwapBound);
+    this.removeEventListener("htmx:afterSwap", this.onAfterSwapBound);
+
+    this.form = null;
+    this.submitButton = null;
+    this.initialFormData = null;
+    this.fields = {};
   }
 
-  onInputChanged() {
-    const submitButton = this.querySelector("#submit-register");
-    if (!submitButton) {
+  onInputChange(event) {
+    event.stopPropagation();
+
+    if (
+      !isFormChanged(this.form, FIELDS, this.initialFormData) ||
+      !isFormValid(this.form, FIELDS)
+    ) {
+      this.submitButton.disabled = true;
+    } else {
+      this.submitButton.disabled = false;
+    }
+  }
+
+  onBeforeSwap(ev) {
+    // redirect
+    const requestPath = ev?.detail?.pathInfo?.requestPath;
+    const responsePath = ev?.detail?.pathInfo?.responsePath;
+    if (responsePath && requestPath !== responsePath) {
+      ev.detail.shouldSwap = false;
+      window.location.replace(responsePath);
       return;
     }
 
-    const firstNameInput = this.querySelector("input[name='customer\[first_name\]']");
-    const firstNameValue = firstNameInput?.value;
-    const lastNameInput = this.querySelector("input[name='customer\[last_name\]']");
-    const lastNameValue = lastNameInput?.value;
-    const emailInput = this.querySelector("input[name='customer\[email\]']");
-    const emailValue = emailInput?.value;
-    const passwordInput = this.querySelector("input[name='customer\[password\]']");
-    const passwordValue = passwordInput?.value;
-   
-     if (emailValue && passwordValue && firstNameValue && lastNameValue) {
-      submitButton.disabled = false;
-    } else {
-      submitButton.disabled = true;
+    // store fields
+    this.fields = {};
+    for (const field of FIELDS) {
+      const value = this.form?.elements[field]?.value || '';
+      this.fields[field] = value;
+    }
+  }
+
+  onAfterSwap() {
+    // restore fields
+    for (const field of FIELDS) {
+      const formField = this.form?.elements[field];
+      if (formField) {
+        formField.value = this.fields[field];
+      }
     }
   }
 }
